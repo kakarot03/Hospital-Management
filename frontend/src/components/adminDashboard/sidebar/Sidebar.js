50a4b7711998fe511163ece2b94f40f49345c8b1
Srivarshan <srivarshan611@gmail.com>
2022-07-23 14:40:28 +0530

    Done

--- frontend/src/components/adminDashboard/sidebar/Sidebar.js
+++ frontend/src/components/adminDashboard/sidebar/Sidebar.js
@@ -1,46 +1,54 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import './Sidebar.css';
 import logo from '../assets/logo.png';
+import { NavLink } from 'react-router-dom';
 
 const Sidebar = ({ sidebarOpen, closeSidebar }) => {
-  return (
-    <div className={sidebarOpen ? 'sidebar_responsive' : ''} id="sidebar">
-      <div className="sidebar__title">
-        <div className="sidebar__img">
-          <img src={logo} alt="logo" />
-          <h1 style={{ marginLeft: '7rem' }}> SHC</h1>
-        </div>
-        <i
-          onClick={() => closeSidebar()}
-          className="fa fa-times"
-          id="sidebarIcon"
-          aria-hidden="true"
-        />
-      </div>
+  const handleClick = (dest) => {
+    window.location.href = window.location.origin + '/admin' + dest;
+  };
 
-      <div className="sidebar__menu">
-        <div className="sidebar__link active_menu_link">
-          <i className="fa fa-home" />
-          <a href="#">Dashboard</a>
+  return (
+    <div className="Sidebar">
+      <div className={sidebarOpen ? 'sidebar_responsive' : ''} id="sidebar">
+        <div className="sidebar__title">
+          <div className="sidebar__img">
+            <img src={logo} alt="logo" />
+            <h1 style={{ marginLeft: '7rem' }}> SHC</h1>
+          </div>
+          <i
+            onClick={() => closeSidebar()}
+            className="fa fa-times"
+            id="sidebarIcon"
+            aria-hidden="true"
+          />
         </div>
-        <h2>Details</h2>
-        <div className="sidebar__link">
-          <i className="fa fa-user-secret" aria-hidden="true" />
-          <a href="#">Patient</a>
-        </div>
-        <div className="sidebar__link">
-          <i className="fa fa-building-o" />
-          <a href="#">Doctor</a>
-        </div>
-        <div className="sidebar__link">
-          <i className="fa fa-wrench" />
-          <a href="#">Appointments</a>
-        </div>
-        <div className="sidebar__link">
-          <i className="fa fa-archive" />
-          <a href="#">Bills</a>
-        </div>
-        {/* <h2>LEAVE</h2>
+
+        <div className="sidebar__menu">
+          <div className="sidebar__link active_menu_link">
+            <i className="fa fa-home" />
+            <a style={{ fontSize: '17px' }} onClick={(e) => handleClick('/')}>
+              Dashboard
+            </a>
+          </div>
+          <h2>Details</h2>
+          <div className="sidebar__link">
+            <i className="fa fa-user-secret" aria-hidden="true" />
+            <a onClick={(e) => handleClick('/patient')}>Patient</a>
+          </div>
+          <div className="sidebar__link">
+            <i className="fa fa-building-o" />
+            <a href="#">Doctor</a>
+          </div>
+          <div className="sidebar__link">
+            <i className="fa fa-wrench" />
+            <a href="#">Appointments</a>
+          </div>
+          <div className="sidebar__link">
+            <i className="fa fa-archive" />
+            <a href="#">Bills</a>
+          </div>
+          {/* <h2>LEAVE</h2>
         <div className="sidebar__link">
           <i className="fa fa-question" />
           <a href="#">Requests</a>
@@ -57,18 +65,19 @@ const Sidebar = ({ sidebarOpen, closeSidebar }) => {
           <i className="fa fa-files-o" />
           <a href="#">Apply for leave</a>
         </div> */}
-        <h2>PAYROLL</h2>
-        <div className="sidebar__link">
-          <i className="fa fa-money" />
-          <a href="#">Payroll</a>
-        </div>
-        <div className="sidebar__link">
-          <i className="fa fa-briefcase" />
-          <a href="#">Paygrade</a>
-        </div>
-        <div className="sidebar__logout">
-          <i className="fa fa-power-off" />
-          <a href="#">Log out</a>
+          <h2>PAYROLL</h2>
+          <div className="sidebar__link">
+            <i className="fa fa-money" />
+            <a href="#">Payroll</a>
+          </div>
+          <div className="sidebar__link">
+            <i className="fa fa-briefcase" />
+            <a href="#">Paygrade</a>
+          </div>
+          <div className="sidebar__logout">
+            <i className="fa fa-power-off" />
+            <a href="#">Log out</a>
+          </div>
         </div>
       </div>
     </div>
--- frontend/src/pages/Admin/Admin.jsx
+++ frontend/src/pages/Admin/Admin.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import './Admin.css';
 import Main from '../../components/adminDashboard/main/Main';
 import Navbar from '../../components/adminDashboard/navbar/Navbar';
 import Sidebar from '../../components/adminDashboard/sidebar/Sidebar';
+import PatientDetails from './PatientDetails';
 
 const Admin = () => {
   const [sidebarOpen, setsidebarOpen] = useState(false);
@@ -14,7 +17,12 @@ const Admin = () => {
   return (
     <div className="container">
       <Navbar sidebarOpen={sidebarOpen} openSidebar={openSidebar} />
-      <Main />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/patient" element={<PatientDetails />} />
+        {/* <Route path="/patient" element={<Login />} />
+          <Route path="/appointment" element={<Patient />} /> */}
+      </Routes>
       <Sidebar sidebarOpen={sidebarOpen} closeSidebar={closeSidebar} />
     </div>
   );
