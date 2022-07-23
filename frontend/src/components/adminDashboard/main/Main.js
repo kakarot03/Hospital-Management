935f75dd8f1098522348b97ed3c26fba04f1e398
Srivarshan <srivarshan611@gmail.com>
2022-07-23 20:10:53 +0530

    Update Main.js

--- frontend/src/components/adminDashboard/main/Main.js
+++ frontend/src/components/adminDashboard/main/Main.js
@@ -3,113 +3,17 @@ import hello from '../assets/hello.svg';
 
 const Main = () => {
   return (
-    <div className="MainAdmin">
-      <main>
-        <div className="main__container">
-          {/* <!-- MAIN TITLE STARTS HERE --> */}
+    <main>
+      <div className="main__container">
+        {/* <!-- MAIN TITLE STARTS HERE --> */}
 
-          <div className="main__title">
-            <img src={hello} alt="hello" />
-            <div className="main__greeting">
-              <h1>Hello Sri</h1>
-              <p>Welcome to your admin dashboard</p>
-            </div>
+        <div className="main__title">
+          <img src={hello} alt="hello" />
+          <div className="main__greeting">
+            <h1>Hello Sri</h1>
+            <p>Welcome to your admin dashboard</p>
           </div>
-
-          {/* <!-- MAIN TITLE ENDS HERE --> */}
-
-          {/* <!-- MAIN CARDS STARTS HERE --> */}
-          <div className="main__cards">
-            <div className="card">
-              <i
-                className="fa fa-user-o fa-2x text-lightblue"
-                aria-hidden="true"
-              />
-              <div className="card_inner">
-                <p className="text-primary-p">Number of Patients</p>
-                <span className="font-bold text-title">178</span>
-              </div>
-            </div>
-
-            <div className="card">
-              <i className="fa fa-calendar fa-2x text-red" aria-hidden="true" />
-              <div className="card_inner">
-                <p className="text-primary-p">Number of Doctors</p>
-                <span className="font-bold text-title">67</span>
-              </div>
-            </div>
-
-            <div className="card">
-              <i
-                className="fa fa-video-camera fa-2x text-yellow"
-                aria-hidden="true"
-              />
-              <div className="card_inner">
-                <p className="text-primary-p">Number of Appointments</p>
-                <span className="font-bold text-title">240</span>
-              </div>
-            </div>
-
-            <div className="card">
-              <i
-                className="fa fa-thumbs-up fa-2x text-green"
-                aria-hidden="true"
-              />
-              <div className="card_inner">
-                <p className="text-primary-p">Number of Users</p>
-                <span className="font-bold text-title">645</span>
-              </div>
-            </div>
-          </div>
-          {/* <!-- MAIN CARDS ENDS HERE --> */}
-
-          {/* <!-- CHARTS STARTS HERE --> */}
-          <div className="charts">
-            <div className="charts__left">
-              <div className="charts__left__title">
-                <div>
-                  <h1>Daily Reports</h1>
-                  <p>Cupertino, California, USA</p>
-                </div>
-                <i className="fa fa-usd" aria-hidden="true" />
-              </div>
-            </div>
-
-            <div className="charts__right">
-              <div className="charts__right__title">
-                <div>
-                  <h1>Stats Reports</h1>
-                  <p>Cupertino, California, USA</p>
-                </div>
-                <i className="fa fa-usd" aria-hidden="true" />
-              </div>
-
-              <div className="charts__right__cards">
-                <div className="card1">
-                  <h1>Income</h1>
-                  <p>$75,300</p>
-                </div>
-
-                <div className="card2">
-                  <h1>Sales</h1>
-                  <p>$124,200</p>
-                </div>
-
-                <div className="card3">
-                  <h1>Users</h1>
-                  <p>3900</p>
-                </div>
-
-                <div className="card4">
-                  <h1>Orders</h1>
-                  <p>1881</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* <!-- CHARTS ENDS HERE --> */}
         </div>
-<<<<<<< HEAD
 
         {/* <!-- MAIN TITLE ENDS HERE --> */}
 
@@ -205,10 +109,6 @@ const Main = () => {
         {/* <!-- CHARTS ENDS HERE --> */}
       </div>
     </main>
-=======
-      </main>
-    </div>
->>>>>>> 50a4b7711998fe511163ece2b94f40f49345c8b1
   );
 };
 
