20fff1c15a7962d3cf57a96b356a81103b7762fb
Srivarshan <srivarshan611@gmail.com>
2023-02-17 17:55:16 +0530

    Change in api url

--- frontend/src/Api/AppointmentRoute.js
+++ frontend/src/Api/AppointmentRoute.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:5000/api/v1/appointment',
-  // baseURL: 'https://sri-health-care.herokuapp.com/api/v1/appointment',
+  // baseURL: 'http://localhost:5000/api/v1/appointment',
+  baseURL: 'https://shc-backend.onrender.com/api/v1/appointment',
 });
--- frontend/src/Api/PatientRoute.js
+++ frontend/src/Api/PatientRoute.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:5000/api/v1/patient',
-  // baseURL: 'https://sri-health-care.herokuapp.com/api/v1/patient',
+  // baseURL: 'http://localhost:5000/api/v1/patient',
+  baseURL: 'https://shc-backend.onrender.com/api/v1/patient',
 });
